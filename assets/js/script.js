59d411e580fc068ac327caf3adaae533f25921c2
elissamaine <emaine97@yahoo.com>
2023-02-20 20:41:25 -0800

    finished html doc

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// variables created by get id
+var viewHighScores = document.querySelector("#view-highscores");
+
+
+
+// click event to view the highscores 
+viewHighScores.addEventListener("click", function(){
+    
+}); 
