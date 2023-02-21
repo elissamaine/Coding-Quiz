0e8fad04a8b9424e0be4f8a1094fa26123e9aece
elissamaine <emaine97@yahoo.com>
2023-02-20 22:17:41 -0800

    added comments to the js fle explaining what code needs to be added

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,23 @@ var viewHighScores = document.querySelector("#view-highscores");
 viewHighScores.addEventListener("click", function(){
     
 }); 
+
+//need a function to hide the different parts of the html that i dont want to see when the page is loaded 
+
+//function that starts quiz
+
+//function that pulls up the questions 
+//turn the varriable questions into interactive buttons that the user can click to answer
+//when a button is clicked it moves on to the next question if question is wrong it will take 10 sec of the timer 
+//when all question are answered then it will run the end quiz function
+
+//count down when the quiz is started when it gets to zero it will run the end quiz function
+
+// function to end quiz that will show the quiz end page that will have the user enter thir initials and it will save thir score and initials into local storage 
+
+// have a function to order the bscores from highest to lowest 
+
+// add event listener to view high scores 
+//add functions for the buttons to either start the quiz again or clear the scores
+
+
