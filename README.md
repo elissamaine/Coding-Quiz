014d6bbdbacd3d984bb2774aba32db16ffaeed88
elissamaine <emaine97@yahoo.com>
2023-02-23 19:17:08 -0800

    fixed bug so questions move to the next and the  scores and initaials are stored in local storage. alsi added comments for the bugs that im unsure of how to fix

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Javascript-Quiz
+# Coding-Quiz
+
+## Description 
+The coding quiz was created to help coding students to prepare for the application process with this short timed quiz that asks the user basic question about coding. The quiz is 60 seconds long and five questions, with a ten second penalty for each incorrect answer. When the five questions are answered or the time runs out the quiz will end and the user will be taken to a page where they can input their initials and save their score onto local storage, and all the highscores will be displayed on the screen.
+
+## ScreenShots
+![screenshot of site]()
+
+## Link to Deployed Application
+[link to deployed site]()
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //variables 
 var questionIndex = 0;
 var time = 60;
-var timer
 
 // variables created with document query selector 
 let startScreen = document.getElementById("start-page");
@@ -11,14 +10,17 @@ var key = document.getElementById("key");
 var answers = document.getElementById("answers");
 var endScreen = document.getElementById("quiz-end-page");
 var initialsInput = document.getElementById('initials');
+console.log(initialsInput)
 var submitButton = document.getElementById("initials-input-btn");
 var scoresScreen = document.getElementById("high-scores-page");
 var timerCountDown = document.getElementById("timer");
 var viewHighScores = document.getElementById('view-highscores');
 var tryAgain = document.getElementById('try-again-btn');
 
-var highScores
-
+var highScores = JSON.parse(localStorage.getItem("highScores"))
+if (highScores == undefined) {
+    localStorage.setItem("highScores", JSON.stringify([]))
+}
 
 //quiz questions
 var questions = [
@@ -57,39 +59,22 @@ function openPage() {
 };
 
 
-
-
-//save scores from the end quiz page 
-function saveHighScore() {
-    var initials = initialsInput.value
-    
-
-    if (initials !== "") {
-        var highScores = JSON.parse(window.localStorage.getItem("highScores"))
-        var newScore = {
-            name: initials,
-            score: time,
-        };
-
-        highScores.push(newScore);
-        
-        window.localStorage.setItem('highScores', JSON.stringify(highScores));
-    }
-
-    showHighScores();
-}
-
 //this function pulls up all the scored that were saved to the 
 function showHighScores() {
-    var highScores =JSON.parse(window.localStorage.getItem(highScores))
-    
+    quizScreen.setAttribute("class", "hide");
+    endScreen.setAttribute("class", "hide");
+    startScreen.setAttribute("class", "hide");
+    scoresScreen.setAttribute("class", "show");
+   
+    var highScores =JSON.parse(localStorage.getItem('highScores'))
+    console.log(highScores)
     highScores.sort(function(a, b) {
         return b.score - a.score;
     });
     
     highScores.forEach(function(score) {
     var liEl = document.createElement('li');
-    liEl.textContent = score.initials + " - " + score.score;
+    liEl.textContent = score.name + " - " + score.score;
 
     var olEl = document.getElementById('display-scores');
     olEl.appendChild(liEl);
@@ -98,15 +83,35 @@ function showHighScores() {
     
 }
 
-// click event to view the highscores 
-viewHighScores.onclick = function() {
-    quizScreen.setAttribute("class", "hide");
-    endScreen.setAttribute("class", "hide");
-    startScreen.setAttribute("class", "hide");
-    scoresScreen.setAttribute("class", "show");
+
+//save scores from the end quiz page 
+function saveHighScore() {
+    var initials = initialsInput.value
+    console.log(initials)
+    
+
+    if (initials !== "") {
+        var highScores = JSON.parse(localStorage.getItem("highScores"))
+        var newScore = {
+            name: initials,
+            score: time,
+        };
+
+        highScores.push(newScore);
+        
+        localStorage.setItem('highScores', JSON.stringify(highScores));
+    }
 
     showHighScores();
+   
 }
+
+submitButton.onclick = saveHighScore
+
+// click event to view the highscores 
+//for some reason the view highscores page
+viewHighScores.onclick = showHighScores;
+
 
 //runs start quiz function when the start button is clicked
 startButton.onclick = function() {
@@ -119,13 +124,11 @@ function stopQuiz() {
     startScreen.setAttribute("class", "hide");
     scoresScreen.setAttribute("class", "hide");
     endScreen.setAttribute("class", "show");
-
 }
 
 //function that will be called when the quiz starts that pulls up the questions from the questions variable
 function showQuestions() {
     var currentQuestion = questions[questionIndex];
-    console.log(currentQuestion)
 
     var questionEl = document.getElementById('quiz-question');
     questionEl.textContent = currentQuestion.question;
@@ -143,31 +146,24 @@ function showQuestions() {
         answerBtns.onclick = answerSelection;
         answers.appendChild(answerBtns);
     });
+    
+};
 
-    function answerSelection() {
-        //takes to seconds off the timer when the wrong button is pressed
-        if (this.value !== questions[questionIndex].answerCorrect) {
-            time = time - 10
-            if (time < 10) {
-                time = 0
-            };
+function answerSelection() {
+    //takes to seconds off the timer when the wrong button is pressed
+    if (this.value !== questions[questionIndex].answerCorrect) {
+    time = time - 10
+    if (time < 10) {
+            time = 0
+        };
             timerCountDown.textContent = time;
             key.textContent = "Incorrect.";
-        } else if(this.value == questions[questionIndex].answerCorrect) {
+    } else if(this.value == questions[questionIndex].answerCorrect) {
             key.textContent = "Correct!";
-        };
-        
-        
     };
-    //when a button is clicked it moves on to the next question
-    //when all question are answered then it will run the stop quiz function
-    //figure out how to get to the next question 
-    answerBtns.onclick = currentQuestion ++;
-
-    if (currentQuestion == questions.length) {
-        stopQuiz();
-    };
-    
+       questionIndex ++; 
+       answers.innerHTML = ''
+        showQuestions();
 };
 
 function startQuiz() {
@@ -177,7 +173,7 @@ function startQuiz() {
     quizScreen.setAttribute("class", "show");
     
     //start timer 
-   timer = setInterval(function(){
+   const timer = setInterval(function(){
      if (time >= 1) {
         timerCountDown.textContent = time;
         time = time - 1;
@@ -185,9 +181,14 @@ function startQuiz() {
         timerCountDown.textContent = "";
         stopQuiz();
     }
-    }, 1000)
+    }, 1000);
+
+    //when it gets to the last question it pushed the time to 0 and im not sure why
     // pull up questions 
-    
+    if (questionIndex == questions.length) {
+        clearInterval(timer);
+
+    };
     showQuestions()
 }
 
@@ -196,9 +197,8 @@ function startQuiz() {
 
  
 //add functions for the buttons to restart the quiz again
-tryAgain.onclick = openPage();
+tryAgain.onclick = openPage
 
-submitButton.onclick = saveHighScore();
 
 
 
